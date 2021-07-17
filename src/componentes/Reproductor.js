326dc0c4efc29b5b9f5c3b2d706f31ef5a9d24ee
Ignasi <i.elgstrom@gmail.com>
2021-07-17 22:37:55 +0200

    Estructura principal reproductor

--- src/componentes/Reproductor.js
+++ src/componentes/Reproductor.js
@@ -1,44 +1,88 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-// import Grid from "@material-ui/core/Grid";
-// import Typography from "@material-ui/core/Typography";
+import React, { useState } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
 import Slider from "@material-ui/core/Slider";
-// import VolumeDown from "@material-ui/icons/VolumeDown";
-// import VolumeUp from "@material-ui/icons/VolumeUp";
-// import Container from "@material-ui/core/Container";
-// import ReactPlayer from "react-player";
 
-export const Reproductor = () => {
-  const useStyles = makeStyles({
-    root: {
-      width: 200,
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import VolumeUpIcon from "@material-ui/icons/VolumeUp";
+import ReactPlayer from "react-player";
+
+const PrettoSlider = withStyles({
+  root: {
+    color: "#007acc",
+    height: 8,
+  },
+  thumb: {
+    height: 20,
+    width: 20,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -6,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
     },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+export const Reproductor = () => {
+  const [reproduccion, setReproduccion] = useState({
+    reproduciendo: true,
   });
+  const { reproduciendo } = reproduccion;
 
+  const reproducirOPausar = () => {
+    setReproduccion({
+      ...reproduccion,
+      reproduciendo: !reproduccion.reproduciendo,
+    });
+  };
   return (
     <>
-      <div className="container footer">
-        {/* <Container maxWidth="md" className="reproductor">
+      <div maxwidth="md" className="reproductor">
         <ReactPlayer
           url="https://soundcloud.com/noname/10-shadow-man-ft-saba-smino-phoelix"
           className="reproductor2"
+          muted={false}
+          playing={reproduciendo}
         />
-      </Container> */}
-
-        <div className="row align-items-center">
-          <div className="col-2">Aqui van los Botones</div>
-          <div className="col-8 sliderMusica">
-            <Slider
-              aria-labelledby="continuous-slider"
-              value={50}
-              className="SliderMusica2"
-            />
+      </div>
+      <div className="container-fluid barraReproduccion">
+        <div className="row  justify-content-center">
+          <div className="col-12 col-sm-2 text-center">
+            Nombre de la cancion
           </div>
-          <div className=" col-2 volumenSlideBar">
-            <Slider
-              aria-labelledby="continuous-slider"
-              className="SliderMusica3"
-            />
+          <div className="col-8 ">
+            <div className="row justify-content-center text-center">
+              <div className="col-12 p-3">
+                <SkipPreviousIcon className="botonBack" />
+                <PlayArrowIcon
+                  className="botonPlay"
+                  onClick={reproducirOPausar}
+                />
+                <SkipNextIcon className="botonNext" />
+              </div>
+            </div>
+            <div className="row justify-content-center align items end">
+              <PrettoSlider min={0} max={100} />
+            </div>
+          </div>
+          <div className="col-sm-2 volumen">
+            <VolumeUpIcon className="volumenIcono" />
+            <PrettoSlider className="volumenSlider" />
           </div>
         </div>
       </div>
