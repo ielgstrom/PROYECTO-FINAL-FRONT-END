cdf849c74777869a1914ed2ca7fc2644d9d163ad
Rallge <geraldinelopz@gmail.com>
2021-07-22 03:45:02 +0200

    Se genero componente Pagina Princial para colocar el Login

--- src/App.js
+++ src/App.js
@@ -1,94 +1,40 @@
+import { useState } from "react";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Reproductor from "./componentes/Reproductor/Reproductor";
-import SidebarIzquierdo from "./componentes/Siderbarizquierdo/SidebarIzquierdo";
-import SidebarDerecho from "./componentes/SidebarDerecho/SidebarDerecho";
-import CancionesFavoritas from "./componentes/CancionesFavoritas/CancionesFavoritas";
-import Perfil from "./componentes/Perfil/Perfil";
-import Matches from "./componentes/Matches/Matches";
-import Principal from "./componentes/Principal/Principal";
-import Chat from "./componentes/Chat/Chat";
+import { Principal } from "./componentes/Principal/Principal";
 function App() {
-  const ListaCancionesPrueba = [
-    {
-      urlsong2:
-        "https://soundcloud.com/theavalanches/interstellar-love-feat-leon",
-      artista: "The Avalanches",
-      título: "Interstelar Love",
-    },
-    {
-      urlsong2: "https://soundcloud.com/nationalxball2018/20-seconds",
-      título: "Darkest Hour",
-      artista: "Sevdaliza",
-    },
-    {
-      urlsong2:
-        "https://soundcloud.com/fiorinien/kate-bush-running-up-that-hill",
-      artista: "Kate Bush",
-      título: "Running Up That Hill",
-    },
-    {
-      urlsong2: "https://soundcloud.com/quelle-chris/graphic-bleed-outs-feat",
-      artista: "Quelle Chris",
-      título: "Graphic Bleeds Outs",
-    },
-    {
-      urlsong2: "https://soundcloud.com/kingprincessmusic/pain",
-      artista: "King Princess",
-      título: "Pain",
-    },
-    {
-      urlsong2:
-        "https://soundcloud.com/mariaarnalimarcelbages-music/el-gran-silencio",
-      artista: "Maria Arnal i Marcel Bagés",
-      título: "El gran silencio",
-    },
-    {
-      urlsong2:
-        "https://soundcloud.com/bovalonmusic/sets/yelle-je-taime-encore-rawd",
-      artista: "Yelle",
-      título: "Je t'aime encore",
-    },
-    {
-      urlsong2: "https://soundcloud.com/nathy-peluso/delito",
-      artista: "Nathy Peluso",
-      título: "DELITO",
-    },
-  ];
+  const [login, setLogin] = useState(true);
+  const cambiar = () => {
+    setLogin(!login);
+  };
   return (
     <>
-      <div className="d-flex flex-column">
-        <div className="d-flex  align-items-stretch paginaPrincipal">
-          <Router className="c">
-            <SidebarIzquierdo className="listaCanciones" />
-            <Switch className="header Main">
-              <Route exact path="/">
-                <Principal />
-              </Route>
-              <Route exact path="/perfil">
-                <Perfil />
-              </Route>
-              <Route exact path="/matches">
-                <Matches />
-              </Route>
-              <Route exact path="/cancionesFavoritas">
-                <CancionesFavoritas
-                  ListaCancionesPrueba={ListaCancionesPrueba}
-                />
-              </Route>
-              <Route exact path="/chat">
-                <Chat />
-              </Route>
-            </Switch>
-            <SidebarDerecho className="ListaAmigos" />
-          </Router>
-        </div>
-        <Reproductor
-          ListaCancionesPrueba={ListaCancionesPrueba}
-          className="barraReproduccion barraDeReproduccion"
-        />
+      {login && <div className="container-xl contenedorPrincipal-Login">
+        <div className="login-center">
+          <form className="form">
+            <label htmlFor="username">Nombre de usuario:</label>
+            <p></p>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+            />
+            <div className="form-group">
+              <p></p>
+              <label htmlFor="password">Contraseña:</label>
+              <p></p>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+              />
+            </div>
+            <p></p>
+            <button className="btn-primary btn-lg" onClick={cambiar}>Iniciar Sesión</button>
+          </form>
       </div>
+      </div>}
+      {!login && <Principal />}
     </>
   );
 }
--- src/componentes/Principal/Principal.js
+++ src/componentes/Principal/Principal.js
@@ -1,51 +1,92 @@
 import React, { useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Reproductor from "../Reproductor/Reproductor";
+import SidebarIzquierdo from "../Siderbarizquierdo/SidebarIzquierdo";
+import SidebarDerecho from "../SidebarDerecho/SidebarDerecho";
+import CancionesFavoritas from "../CancionesFavoritas/CancionesFavoritas";
+import Perfil from "../Perfil/Perfil";
+import Matches from "../Matches/Matches";
+import Chat from "../Chat/Chat";
 export const Principal = () => {
-  // const [verFavoritas, setVerFavoritas] = useState(false);
-  // const viewCancionesFavoritas = () => {
-  //   setVerFavoritas(!verFavoritas);
-  // };
-  // const [listaAReproducir, setListaAReproducir] =
-  //   useState(ListaCancionesPrueba);
-  // const reproducirLista = (List) => {
-  //   setListaAReproducir(List);
-  // };
+  const ListaCancionesPrueba = [
+    {
+      urlsong2:
+        "https://soundcloud.com/theavalanches/interstellar-love-feat-leon",
+      artista: "The Avalanches",
+      título: "Interstelar Love",
+    },
+    {
+      urlsong2: "https://soundcloud.com/nationalxball2018/20-seconds",
+      título: "Darkest Hour",
+      artista: "Sevdaliza",
+    },
+    {
+      urlsong2:
+        "https://soundcloud.com/fiorinien/kate-bush-running-up-that-hill",
+      artista: "Kate Bush",
+      título: "Running Up That Hill",
+    },
+    {
+      urlsong2: "https://soundcloud.com/quelle-chris/graphic-bleed-outs-feat",
+      artista: "Quelle Chris",
+      título: "Graphic Bleeds Outs",
+    },
+    {
+      urlsong2: "https://soundcloud.com/kingprincessmusic/pain",
+      artista: "King Princess",
+      título: "Pain",
+    },
+    {
+      urlsong2:
+        "https://soundcloud.com/mariaarnalimarcelbages-music/el-gran-silencio",
+      artista: "Maria Arnal i Marcel Bagés",
+      título: "El gran silencio",
+    },
+    {
+      urlsong2:
+        "https://soundcloud.com/bovalonmusic/sets/yelle-je-taime-encore-rawd",
+      artista: "Yelle",
+      título: "Je t'aime encore",
+    },
+    {
+      urlsong2: "https://soundcloud.com/nathy-peluso/delito",
+      artista: "Nathy Peluso",
+      título: "DELITO",
+    },
+  ];
   return (
     <>
-      {/* <div className="d-flex flex-column ">
-        <div className="d-flex  align-items-stretch paginaPrincipal">
-          <aside className=" section listaCanciones">
-            <form className=" d-flex flex-row">
-              <input
-                type="text"
-                className="form-control form-control-md buscadorMusica"
-                placeholder="Buscar Musica"
-              ></input> */}
-      {/* <i className="buscadorMusica">B</i>
-            </form>
-            <button
-              type="button"
-              className="btnBuscar btn btn-primary"
-              onClick={viewCancionesFavoritas}
-            >
-              Canciones favoritas
-            </button>
+      <>
+        <div className="d-flex flex-column">
+          <div className="d-flex  align-items-stretch paginaPrincipal">
+            <Router className="c">
+              <SidebarIzquierdo className="listaCanciones" />
+              <Switch className="header Main">
+                <Route exact path="/perfil">
+                  <Perfil />
+                </Route>
+                <Route exact path="/matches">
+                  <Matches />
+                </Route>
+                <Route exact path="/cancionesFavoritas">
+                  <CancionesFavoritas
+                    ListaCancionesPrueba={ListaCancionesPrueba}
+                  />
+                </Route>
+                <Route exact path="/chat">
+                  <Chat />
+                </Route>
+              </Switch>
+              <SidebarDerecho className="ListaAmigos" />
+            </Router>
+          </div>
+          <Reproductor
+            ListaCancionesPrueba={ListaCancionesPrueba}
+            className="barraReproduccion barraDeReproduccion"
+          />
+        </div>
+      </>
 
-            <h3 className="historialTiulo">Historial</h3>
-            <div className="lorem">
-              <HistorialSidebar historial={ListaCancionesPrueba} />
-            </div>
-          </aside> */}
-
-      <p className="header Main"></p>
-      {/* {true && <Perfil />} */}
-      {/* {verFavoritas && (
-              <CancionesFavoritas ListaCancionesPrueba={ListaCancionesPrueba} />
-            )}
-          </p>
-
-
-      </div> */}
     </>
   );
 };
