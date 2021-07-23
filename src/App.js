8cb7381496204c7d24818d86019d2b6d1ba89475
Rallge <geraldinelopz@gmail.com>
2021-07-23 07:28:27 +0200

    Se creo el componente login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import React from "react";
 import { Principal } from "./componentes/Principal/Principal";
+import { Login } from "./componentes/Login/Login";
 import { Register } from "./componentes/Register/Register";
 function App() {
   const [login, setLogin] = useState(true);
@@ -14,59 +15,8 @@ function App() {
 
   return (
     <>
-      {!login && (
-        <div className="container-xl contenedorPrincipal-Login">
-          <div className="login-center">
-            <form className="form">
-              <label htmlFor="username">Nombre de usuario:</label>
-              <p></p>
-              <input type="text" className="form-control" id="username" />
-              <div className="form-group">
-                <p></p>
-                <label htmlFor="password">Contraseña:</label>
-                <p></p>
-                <input type="password" id="password" className="form-control" />
-              </div>
-              <div className="form-group">
-                <label
-                  htmlFor="recordarPassword"
-                  className="form-check-label fw-bold"
-                >
-                  ¿Has olvidado tu contraseña?
-                </label>
-              </div>
-              <p></p>
-              <div className="boton-login ">
-                <button
-                  className="btn-primary btn-lg"
-                  onClick={cambiarAPaginaPrincipal}
-                >
-                  Iniciar Sesión
-                </button>
-              </div>
-              <hr className="linea-divisoria"></hr>
-              <div className="form-group">
-                <label
-                  htmlFor="recordarPassword"
-                  className="form-check-label fw-bold"
-                >
-                  ¿No tienes cuenta?
-                </label>
-              </div>
-              <div className="boton-login">
-                <button
-                  className="btn-primary btn-lg "
-                  onClick={cambiarARegister}
-                >
-                  {" "}
-                  Registrarme <i className="icon-logo"></i>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-      {login && <Principal />}
+      {login && <Login />}
+      {!login && <Principal />}
       {!register && <Register />}
     </>
   );
