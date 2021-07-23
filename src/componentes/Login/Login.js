7644635503b633d88258d7348e743362ca7644fe
Rallge <geraldinelopz@gmail.com>
2021-07-23 07:57:32 +0200

    Implementacion de error de usuario invalido

--- src/componentes/Login/Login.js
+++ src/componentes/Login/Login.js
@@ -1,5 +1,37 @@
+import { useState } from "react";
 import "./Login.css";
-export const Login = () => {
+
+export const Login = (props) => {
+  const { setLogin } = props;
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const entrar = async (username, password) => {
+    const credencials = { username, password };
+    const resp = await fetch(
+      "https://myrythm.herokuapp.com/usuario/login",
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credencials),
+      }
+    );
+    if (resp.ok) {
+      setError(false);
+      const { token } = await resp.json();
+      localStorage.setItem("token", token);
+      localStorage.setItem("login", true);
+      setLogin(true);
+    } else {
+      setError(true);
+      setLogin(false);
+    }
+  };
+
+
   return (
     <>
       <div className="container-xl contenedorPrincipal-Login">
@@ -7,12 +39,27 @@ export const Login = () => {
           <form className="form">
             <label htmlFor="username">Nombre de usuario:</label>
             <p></p>
-            <input type="text" className="form-control" id="username" />
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              id="username"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
             <div className="form-group">
               <p></p>
               <label htmlFor="password">Contraseña:</label>
               <p></p>
-              <input type="password" id="password" className="form-control" />
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                name="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                 />
             </div>
             <div className="form-group">
               <label
@@ -26,10 +73,14 @@ export const Login = () => {
             <div className="boton-login ">
               <button
                 className="btn-primary btn-lg"
-                /*onClick={cambiarAPaginaPrincipal}*/
+                onClick={(e) => {
+                  e.preventDefault();
+                  entrar(username, password);
+                }}
               >
                 Iniciar Sesión
               </button>
+              {error && <p>Error d'autentificació</p>}
             </div>
             <hr className="linea-divisoria"></hr>
             <div className="form-group">
@@ -43,7 +94,7 @@ export const Login = () => {
             <div className="boton-login">
               <button
                 className="btn-primary btn-lg "
-                /*onClick={cambiarARegister}*/
+              /*onClick={cambiarARegister}*/
               >
                 {" "}
                 Registrarme <i className="icon-logo"></i>
